ed5a05e388124ee715982db8fab662db40bb66f4
slymbo <hatemslimiofficial@gmail.com>
2022-07-19 23:20:30 +0200

    Scaffolding package

---
+++ src/Models/Comment.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace Slymbo\Laraposts\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Slymbo\Laraposts\Database\Factories\PostFactory;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'comments';
+
+    /**
+     * @var array
+     */
+    protected $fillable = [];
+
+    /**
+     * @return PostFactory
+     */
+    protected static function newFactory()
+    {
+        return PostFactory::new();
+    }
+
+}
